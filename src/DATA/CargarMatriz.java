6b556f54141d008e9789a68b1bc2c279e733c288
DarcherDev <daniel_aristizabal@outlook.com>
2022-04-24 18:25:01 -0500

    cargar de archivos proyecto piesApp

---
+++ src/DATA/CargarMatriz.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DATA;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author usuario
+ */
+public class CargarMatriz {
+
+    Matriz sudoku;
+    String SudokuString[][] = new String[9][9];
+
+    public CargarMatriz(Matriz sudoku) {
+        this.sudoku = sudoku;
+    }
+
+    /**
+     * Metodo para Leer el archivo de texto con la configuracion inicial
+     *
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public void Leer() throws FileNotFoundException, IOException {
+        try {
+            // creamos el archivo
+            FileReader archivo = new FileReader(new File("ConfiguracionInicial.txt"));
+            //creamos el BufferReader para leer
+            BufferedReader lector = new BufferedReader(archivo);
+            String linea;
+            int numlinea = 0;
+            while ((linea = lector.readLine()) != null) {
+                String fila[] = linea.split("");
+                for (int i = 0; i < fila.length; i++) {
+                    SudokuString[numlinea][i] = fila[i];
+                    System.out.println(SudokuString[numlinea][i]);
+                }
+                numlinea++;
+            }
+            linea = lector.readLine();
+            // ya temenos la matriz en string y la pasamos a entero para empezar a resolver
+            
+            sudoku.setSudokuString(SudokuString);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/DATA/Matriz.java
@@ -0,0 +1,83 @@
+package DATA;
+
+import static java.lang.Integer.SIZE;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author usuario
+ */
+public class Matriz {
+
+    private int SIZE = 9;
+    private int matriz[][] = new int[SIZE][SIZE];
+    private String SudokuString[][] = new String[9][9];
+    private CargarMatriz cargarMatriz;
+
+    public Matriz() {
+        cargarMatriz = new CargarMatriz(this);
+    }
+
+    public Matriz(int[][] matriz) {
+    }
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+    public void resulverMatriz() {
+
+    }
+
+    public void convertirStringAInt() {
+
+    }
+
+    public void cargarMatriz() {
+        convertirStringAInt();
+        resulverMatriz();
+    }
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(int[][] matriz) {
+        this.matriz = matriz;
+    }
+
+    public int getSIZE() {
+        return SIZE;
+    }
+
+    public void setSIZE(int SIZE) {
+        this.SIZE = SIZE;
+    }
+
+    public String[][] getSudokuString() {
+        return SudokuString;
+    }
+
+    public void setSudokuString(String[][] SudokuString) {
+        this.SudokuString = SudokuString;
+    }
+
+    public CargarMatriz getCargarMatriz() {
+        return cargarMatriz;
+    }
+
+    public void setCargarMatriz(CargarMatriz cargarMatriz) {
+        this.cargarMatriz = cargarMatriz;
+    }
+}
